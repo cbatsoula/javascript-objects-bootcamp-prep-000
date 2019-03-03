dd5603639a81326034ec9cdbf818dc45f3aad57e
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-03 04:56:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(Playlist, artistName, songTitle){
   
 }
 function removeFromPlaylist(playlist, artistName){
-  delete Slowdive.artistName;
+  delete playlist.artistName ;
   return playlist;
 }
