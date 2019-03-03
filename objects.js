5685cef58bcf291e23e6de2138b9b8090a1ffc09
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-03 05:15:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs'] = "Here's to the State of Mississippi";
   return Object.assign({}, playlist, { [artistName]: songTitle });
 }
+function removeFromPlaylist(playlist, artistName){
+  delete
+}
